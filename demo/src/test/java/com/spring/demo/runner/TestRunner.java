faa8ef34471f5f66e5e49a555632a14ba5965996
akoka <akoka@altimetrik.com>
2024-08-13 17:45:16 +0530

    commit 13082024

--- demo/src/test/java/com/spring/demo/runner/TestRunner.java
+++ demo/src/test/java/com/spring/demo/runner/TestRunner.java
@@ -1,7 +1,7 @@
 package com.spring.demo.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-
+import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(
@@ -16,4 +16,11 @@ import io.cucumber.testng.CucumberOptions;
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
+
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
