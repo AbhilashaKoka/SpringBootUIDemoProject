d97e14921a379339c489f392bd3197d69b5712d3
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2026-04-01 22:00:02 +0530

    test_03

--- demo/src/main/java/com/spring/demo/libraries/WebDriverLibrary.java
+++ demo/src/main/java/com/spring/demo/libraries/WebDriverLibrary.java
@@ -2,13 +2,13 @@ package com.spring.demo.libraries;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
-
 
 
 @Configuration
@@ -20,7 +20,18 @@ public class WebDriverLibrary {
     @Scope("driverScope")
     public WebDriver getChromeDriver(){
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        // Enable headless mode if CHROME_HEADLESS environment variable is set
+        String headlessEnv = System.getenv("CHROME_HEADLESS");
+        if ("true".equalsIgnoreCase(headlessEnv)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+        }
+
+        return new ChromeDriver(options);
     }
 
     @Bean
